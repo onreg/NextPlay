a91c309c75baf96e16b692871d1675f795eca496
Vadzim Korzun <Onreg01@gmail.com>
2025-12-27 17:03:13 +0100

    Adds preview functions for GameList component and enhances ThemePreview annotations for tablet support

--- core/ui/src/main/kotlin/io/github/onreg/core/ui/preview/ThemePreview.kt
+++ core/ui/src/main/kotlin/io/github/onreg/core/ui/preview/ThemePreview.kt
@@ -1,6 +1,7 @@
 package io.github.onreg.core.ui.preview
 
 import android.content.res.Configuration
+import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 
 
@@ -17,3 +18,18 @@ import androidx.compose.ui.tooling.preview.Preview
     uiMode = Configuration.UI_MODE_NIGHT_YES
 )
 public annotation class ThemePreview()
+
+@Preview(
+    name = "Tablet - Light",
+    device = Devices.TABLET,
+    showBackground = true,
+    backgroundColor = 0xFFFFFFFF
+)
+@Preview(
+    name = "Tablet - Dark",
+    device = Devices.TABLET,
+    showBackground = true,
+    backgroundColor = 0xFF000000,
+    uiMode = Configuration.UI_MODE_NIGHT_YES
+)
+public annotation class TabletThemePreview()
