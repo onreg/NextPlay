bc04bd9403dd79a1c797460e2bc7ee1056d3845e
Vadzim Korzun <Onreg01@gmail.com>
2026-02-21 16:22:48 +0100

    Refactor navigation and update package structure for game details

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,6 +17,12 @@ dependencies {
     implementation(projects.core.ui)
     implementation(projects.core.db)
     implementation(projects.core.network)
-    implementation(projects.feature.game)
+
+    implementation(projects.feature.gameList)
+    implementation(projects.feature.gameDetails)
+
     implementation(projects.data.game.impl)
+    implementation(projects.data.details.impl)
+    implementation(projects.data.screenshots.impl)
+    implementation(projects.data.movies.impl)
 }
